277c91eb3cbbcfd6bd1bb7d9b94ee835effaeea8
a22403871RodrigoAlexandre <a22403871@alunos.ulht.pt>
2026-04-22 17:45:19 +0000

    Atualização do urls.py

--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+## ficheiro projects/urls.py
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include              #     <- adicionar include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("escola/", include("escola.urls")),       #     <- rota para aplicação web escola
 ]
+
+# Serve ficheiros media em DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
