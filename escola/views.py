14a5d551e778d65251b1db1049ae33facc9a1bff
a22403871RodrigoAlexandre <a22403871@alunos.ulht.pt>
2026-04-22 18:23:07 +0000

    Atualizacao dos templates

--- escola/views.py
+++ escola/views.py
@@ -1,13 +1,12 @@
-##  ficheiro escola/views.py
-
 from django.shortcuts import render
 from .models import Curso
 
 def cursos_view(request):
-
-    cursos = Curso.objects
-               .select_related('professor')
-               .prefetch_related('alunos')
-               .all()
+    cursos = (
+        Curso.objects
+        .select_related('professor')
+        .prefetch_related('alunos')
+        .all()
+    )
     
     return render(request, 'escola/cursos.html', {'cursos': cursos})
